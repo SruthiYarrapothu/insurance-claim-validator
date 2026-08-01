dcbd98f41c45f5509aaf26e19ae5329b543d4323
Sruthi <yarrapothu.sruthi@accenture.com>
2026-08-01 19:15:50 +0530

    Convert backend from submodule to regular files

---
+++ insurance-claim-validation/src/main/java/com/accenture/insuranceclaimvalidation/controller/ClaimController.java
@@ -0,0 +1,33 @@
+package com.accenture.insuranceclaimvalidation.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.accenture.insuranceclaimvalidation.dto.response.FileUploadResponse;
+import com.accenture.insuranceclaimvalidation.service.ClaimService;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api/v1/claims")
+@Validated
+public class ClaimController {
+
+    private final ClaimService claimService;
+
+    public ClaimController(ClaimService claimService) {
+        this.claimService = claimService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<FileUploadResponse> uploadClaim(
+            @RequestParam("file") MultipartFile file) {
+
+        return ResponseEntity.ok(claimService.uploadClaim(file));
+    }
+}
